d34b3e0bc2b3cd7e925c9c3d38f436cb2e3e6b00
marmooo <marmooo@users.noreply.github.com>
2025-12-15 01:26:18 +0900

    add service worker

---
+++ src/sw.js
@@ -0,0 +1,40 @@
+const cacheName = "2025-12-15 00:00";
+const urlsToCache = [
+  "/timidy/index.js",
+  "/timidy/favicon/favicon.svg",
+  "https://marmooo.github.io/midi-player/midi-player-icons.woff2",
+  "https://soundfonts.pages.dev/GeneralUser_GS_v1.471/000.sf3",
+];
+
+async function preCache() {
+  const cache = await caches.open(cacheName);
+  await Promise.all(
+    urlsToCache.map((url) =>
+      cache.add(url).catch((err) => console.warn("Failed to cache", url, err))
+    ),
+  );
+  self.skipWaiting();
+}
+
+async function handleFetch(event) {
+  const cached = await caches.match(event.request);
+  return cached || fetch(event.request);
+}
+
+async function cleanOldCaches() {
+  const cacheNames = await caches.keys();
+  await Promise.all(
+    cacheNames.map((name) => name !== cacheName ? caches.delete(name) : null),
+  );
+  self.clients.claim();
+}
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(preCache());
+});
+self.addEventListener("fetch", (event) => {
+  event.respondWith(handleFetch(event));
+});
+self.addEventListener("activate", (event) => {
+  event.waitUntil(cleanOldCaches());
+});
