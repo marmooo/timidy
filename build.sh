c2425b53d39523205aebbba6edcb9ab78c6191cb
marmooo <marmooo@users.noreply.github.com>
2026-08-11 10:57:56 +0900

    fix build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 mkdir -p docs
 cp -r src/* docs
 drop-inline-css -r src -o docs
-deno bundle --allow-import src/index.js -o docs/index.js
+deno bundle --reload --allow-import src/index.js -o docs/index.js
 minify -r docs -o .
